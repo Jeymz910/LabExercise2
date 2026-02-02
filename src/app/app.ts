e7bd825aac31f486e1dbc528b63e3ec4750345cf
James Barcatan <barcatanjames204@gmail.com>
2026-02-02 12:12:41 +0800

    Initial commit of Angular project

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],  // Empty, since RouterOutlet was unused
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = ('J-TECH SHOP');
+}
+
